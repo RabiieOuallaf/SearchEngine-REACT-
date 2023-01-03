4becac7732e06a584bf7051a2cc36a1744dccbf9
RabiieOuallaf <rabie.ouallaf400@hotmail.com>
2023-01-03 13:48:55 +0100

    Add footer 🦶

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,11 @@ const Footer = () => {
 
   return (
 
-    <div>Footer</div>
+    <div className='text-center p-10 mt-10 border-t dark:border-gray-700 border-gray-200'>
+      
+      <h1>2022@Rabie ouallaf</h1>
+
+    </div>
     
   )
 
